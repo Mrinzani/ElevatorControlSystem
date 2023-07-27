54b67b9b7f1886e85cd5c9fef75e44697bf78a01
Alexey Salahutdinov <Alekse-daf@yandex.ru>
2023-07-27 15:29:46 +0300

    Добавьте файлы проекта.

---
+++ ElevatorControlSystem/ElevatorCab.cs
@@ -0,0 +1,43 @@
+﻿namespace ElevatorControlSystem
+{
+    public class ElevatorCab : IElevatorCab
+    {
+        public int CurrentPosition { get; set; }
+        public string Status { get; set; }
+
+        public ElevatorCab()
+        {
+
+        }
+
+        public void ChooseFloorButton()
+        {
+
+        }
+        public void OpenDoor()
+        {
+
+        }
+
+        public void CloseDoor() 
+        { 
+        
+        }
+
+        public void СallingOperator()
+        {
+
+        }
+
+        public void MovementBetweenDoor()
+        {
+
+        }
+
+        public void NoMovementBetweenDoor()
+        {
+
+        }
+
+    }
+}
---
+++ ElevatorControlSystem/IElevatorCab.cs
@@ -0,0 +1,14 @@
+﻿namespace ElevatorControlSystem
+{
+    public interface IElevatorCab
+    {
+        int CurrentPosition { get; }
+        string Status { get; }
+        public void ChooseFloorButton();
+        public void OpenDoor();
+        void CloseDoor();
+        void СallingOperator();
+        void MovementBetweenDoor();
+        void NoMovementBetweenDoor();
+    }
+}
